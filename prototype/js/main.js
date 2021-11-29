2638632e1a39af1c27e7baf9cbcb93b0fe103b14
Darsh978 <d.narain@rgu.ac.uk>
2021-11-29 16:59:38 +0000

    Add files via upload

--- prototype/js/main.js
+++ prototype/js/main.js
@@ -6,7 +6,7 @@
  */
 
 
-
+//Darsh's Code Start
 //set the map and initial coordinates
 
 var map = L.map('mapid').setView([0, 0], 1); 
@@ -16,7 +16,7 @@ var map = L.map('mapid').setView([0, 0], 1);
 
                 // Making a map and tiles 
 
-                L.marker([51.505, 20]).addTo(map); 
+               // L.marker([51.505, 20]).addTo(map); 
 
                  //  Making a marker with a custom icon 
 
@@ -38,23 +38,42 @@ var map = L.map('mapid').setView([0, 0], 1);
                      accessToken: 'pk.eyJ1IjoiZGFyc2g5NzgiLCJhIjoiY2t3Z3lwYTluMGFjNzJvbzB3Ym1teW1uaiJ9.Qw_1snRS2-UPzTfIE-0rRA'
                  }).addTo(map);
 
+                 //use geolocate to find get home users exact location in map 
+                
+                 L.control.locate().addTo(map);
+
+                 //search in map 
+
+                L.Control.geocoder().addTo(map); 
+
+            
                 // var tiles = L.tileLayer(tileUrl, { attribution }); 
                 
+         
 
 //when the button on is clicked
 $('#CBtn').click(function() {
-    console.log("getting quakes");
+    console.log("getting charging points");
     //use the jquery get json method to retrieve our json
+    //http://chargepoints.dft.gov.uk/api/retrieve/registry/postcode/EC3A+7BR/limit/10/
+     //https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson
+
+
+
+
     $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function(result) {
         //response data is now in the result variable
         //uncomment the line below to see the raw json, this would let you see the structure of the response
-        //console.log(result)
+        console.log(result)
         //I know that the earthquakes are defined in an array (or list) result.features
         result.features.forEach(function(quake) {
           //for each earthquake
           //get its coordinates
           var lng = quake.geometry.coordinates[0];
           var lat = quake.geometry.coordinates[1];
+
+          //var lng = quake.ChargeDeiceLocation.longitude[0];
+          //var lat = quake.ChargeDeiceLocation.Latitude[1];
           //and it magnitude
           var mag = quake.properties.mag * 50;
           //for each earthquake create a circle
@@ -67,13 +86,46 @@ $('#CBtn').click(function() {
             })
             //and add it to the map
             circle.addTo(map);
-
+            L.marker([lat, lng]).addTo(map); 
         });
 
     });
-    // $.getJSON("http://www.kuakes.com/json/?callback=?", function(result) {
-    //   console.log(result); 
-    // });
-
 });
 
+    
+//when the button on is clicked
+$('#EBtn').click(function() {
+    console.log("testing data")
+    //use the jquery get json method to retrieve our json
+    //https://cors-anywhere.herokuapp.com/http://chargepoints.dft.gov.uk/api/retrieve/registry/postcode/EC3A+7BR/limit/10/
+     //https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson
+
+
+     //https://cors-anywhere.herokuapp.com/http://
+     //https://api.carbonintensity.org.uk/regional/scotland
+
+    $.getJSON("https://cors-anywhere.herokuapp.com/http://chargepoints.dft.gov.uk/api/retrieve/registry/postcode/AB15/limit/10/format/json/", function(result) {
+        //response data is now in the result variable
+        //uncomment the line below to see the raw json, this would let you see the structure of the response
+        console.log(result);
+        //I know that the earthquakes are defined in an array (or list) result.features
+       
+       result.ChargeDevice.forEach(function(cp) {
+            //for each charging point
+            //get its coordinates
+            var lng = cp.ChargeDeviceLocation.Longitude;
+            var lat = cp.ChargeDeviceLocation.Latitude;
+            L.marker([lat, lng]).addTo(map); 
+            console.log(lng)
+            console.log(lat)
+            console.log("plot the point data")
+});
+
+
+    });
+
+});
+    
+   
+
+//Darsh's Code End
