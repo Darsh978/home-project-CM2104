a1b2509237eef078db9d4e31b00c6f99064c78f5
Darsh978 <d.narain@rgu.ac.uk>
2021-12-01 14:37:08 +0000

    Add files via upload

--- prototype/js/main.js
+++ prototype/js/main.js
@@ -52,8 +52,8 @@ var map = L.map('mapid').setView([0, 0], 1);
          
 
 //when the button on is clicked
-$('#CBtn').click(function() {
-    console.log("getting charging points");
+$('#EBtn').click(function() {
+    console.log("getting carbon intensity data");
     //use the jquery get json method to retrieve our json
     //http://chargepoints.dft.gov.uk/api/retrieve/registry/postcode/EC3A+7BR/limit/10/
      //https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson
@@ -61,41 +61,54 @@ $('#CBtn').click(function() {
 
 
 
-    $.getJSON("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson", function(result) {
+    $.getJSON("https://api.carbonintensity.org.uk/regional/scotland", function(result) {
         //response data is now in the result variable
         //uncomment the line below to see the raw json, this would let you see the structure of the response
         console.log(result)
-        //I know that the earthquakes are defined in an array (or list) result.features
-        result.features.forEach(function(quake) {
+
+        var region= result.data[0].shortname
+         console.log(region)
+
+         var forecast= result.data[0].data[0].intensity.forecast
+         console.log(forecast)
+        
+         var index= result.data[0].data[0].intensity.index
+         console.log(index)
+        
+        
+        //result.data.data.generationmix.forEach(function(fuel) {
           //for each earthquake
           //get its coordinates
-          var lng = quake.geometry.coordinates[0];
-          var lat = quake.geometry.coordinates[1];
+         // var lng = quake.geometry.coordinates[0];
+         // var lat = quake.geometry.coordinates[1];
 
           //var lng = quake.ChargeDeiceLocation.longitude[0];
           //var lat = quake.ChargeDeiceLocation.Latitude[1];
           //and it magnitude
-          var mag = quake.properties.mag * 50;
-          //for each earthquake create a circle
-            var circle = L.circle([lat, lng],mag*50, {
-                color: 'blue',
-                opacity:0,
-                fillColor: 'blue', 
-                fillOpacity: 0.8,
-                //radius: mag
-            })
+         
+
+        
+         
+         //for each earthquake create a circle
+           // var circle = L.circle([lat, lng],mag*50, {
+            //    color: 'blue',
+            //    opacity:0,
+            //    fillColor: 'blue', 
+            //    fillOpacity: 0.8,
+            //    //radius: mag
+           // })
             //and add it to the map
-            circle.addTo(map);
-            L.marker([lat, lng]).addTo(map); 
-        });
+           // circle.addTo(map);
+           // L.marker([lat, lng]).addTo(map); 
+        //});
 
     });
 });
 
     
 //when the button on is clicked
-$('#EBtn').click(function() {
-    console.log("testing data")
+$('#CBtn').click(function() {
+    console.log("Getting Charging points")
     //use the jquery get json method to retrieve our json
     //https://cors-anywhere.herokuapp.com/http://chargepoints.dft.gov.uk/api/retrieve/registry/postcode/EC3A+7BR/limit/10/
      //https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson
@@ -119,7 +132,7 @@ $('#EBtn').click(function() {
             console.log(lng)
             console.log(lat)
             console.log("plot the point data")
-});
+        });
 
 
     });
